9f4ceb246b0e5abcd8f3800bf47c22791e9363c7
neelasatyasai <neelasatyasai9.3@gmail.com>
2025-06-01 14:46:42 +0530

    used useRef

---
+++ xstopwatch/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Stopwatch from './Stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Stopwatch></Stopwatch>
+    </div>
+  );
+}
+
+export default App;
---
+++ xstopwatch/src/Stopwatch.jsx
@@ -0,0 +1,49 @@
+import React, { useRef, useState } from 'react'
+
+function Stopwatch() {
+  
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsed, setElapsed] =  useState(0);
+  const intervalRef = useRef(null);
+
+  function format(){
+     
+    let min = Math.trunc(elapsed/60);
+    let sec =  elapsed%60;
+    return `Time:${min}:${sec <10 ? "0": ""}${sec}`
+  }
+  
+  function reset(){
+     clearInterval(intervalRef.current);
+    setElapsed(0);
+    setIsRunning(false);
+  }
+
+  function startStop(){
+      if(isRunning){
+         clearInterval(intervalRef.current);
+
+         setIsRunning(false);
+      }
+      else{
+         
+         setIsRunning(true);
+         intervalRef.current  = setInterval(()=>{
+             setElapsed((prev)=> prev+1 );
+         }, 1000);
+      }
+  }
+
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <h3>{format()}</h3>
+      <button onClick={startStop}>{isRunning? "Stop": "Start"}</button>
+      <button onClick={reset}>Reset</button>
+
+    </div>
+  )
+}
+
+export default Stopwatch
